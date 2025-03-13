689921251b8960293f4f2bf786b7e03671b542d5
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-13 23:52:55 +0530

    Add table template to the BookConsole

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Nav } from 'react-bootstrap';
 import NavB from './components/Nav';
+import { BookConsole } from './components/BookConsole';
 
 function App() {
   return (
     <>
       <NavB/>
+      <BookConsole/>
     </>
   );
 }
