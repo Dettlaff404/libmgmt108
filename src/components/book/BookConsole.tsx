2c8b636ab9939941e4926ebedc7d1c9735d613bf
Ashan Randima Manamperi <120651088+Dettlaff404@users.noreply.github.com>
2025-03-21 11:42:31 +0530

    Optimized service layer for all CRUDs in Book

--- src/components/book/BookConsole.tsx
+++ src/components/book/BookConsole.tsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import EditBook from './EditBook';
 import AddBook from './AddBook';
-import { AddBookData, UpdateBook, GetBooks, DeleteBook } from '../../service/books/BookData';
+import { AddBookData, UpdateBook, GetBooks, DeleteBook } from '../../service/BookData';
 
 export function BookConsole() {
 
